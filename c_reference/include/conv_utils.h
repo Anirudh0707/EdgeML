392cf2e33b073c5c1aaa56df731f71fb6d7cd0bf
Anirudh B H <t-abh@microsoft.com>
2020-10-05 20:02:16 +0530

    Test for vanilla Conv1d layer

--- c_reference/include/conv_utils.h
+++ c_reference/include/conv_utils.h
@@ -5,5 +5,7 @@
 #include <float.h>
 
 int prepareLowRankConvMat(float* out, float* W1, float* W2, unsigned rank, unsigned I, unsigned J);
+float sigmoid(float x);
+float relu(float x);
 
 #endif
--- c_reference/src/conv_utils.c
+++ c_reference/src/conv_utils.c
@@ -3,13 +3,22 @@
 #include <float.h>
 
 int prepareLowRankConvMat(float* out, float* W1, float* W2, unsigned rank, unsigned I, unsigned J){
-    for(i = 0 ; i < I, i++){
-        for(j = 0 ; j < J, j++){
+    for(int i = 0 ; i < I; i++){
+        for(int j = 0 ; j < J; j++){
             float sum = 0;
-            for(k = 0; k < rank ; k++){
+            for(int k = 0; k < rank ; k++){
                 sum += (W1[i * rank + k] * W2[k * J + j]);
             }
             out[i * J + j] = sum;
         }
     }
+}
+
+float relu(float x) {
+  if (x < 0.0) return 0.0;
+  else return x;
+}
+
+float sigmoid(float x) {
+  return 1.0f / (1.0f + expf(-1.0f * x));
 }
