a6d883664ba3ed64dc31bf7529e1900dc2183363
Anirudh0707 <yani.bh@gmail.com>
2020-10-07 18:05:09 +0530

    add epsilon to variance to avoid division by zero

--- c_reference/src/conv1d.c
+++ c_reference/src/conv1d.c
@@ -212,18 +212,19 @@ int AvgPool1D(float *output_signal, unsigned out_T, const float *input_signal, u
 
 int BatchNorm1d(float* output_signal, float* input_signal, unsigned in_T, unsigned in_channels, 
     float* mean, float* var, unsigned affine, float* gamma , float * beta, unsigned in_place){
+    float eps = 0.00001;
     if(affine){
         if(in_place){
             for(int t = 0; t < in_T ; t++){
                 for(int d = 0 ; d < in_channels ; d++){
-                    input_signal[t * in_channels + d]  = gamma[d]*((input_signal[t * in_channels + d] - mean[d])/sqrt(var[d])) + beta[d];
+                    input_signal[t * in_channels + d]  = gamma[d]*((input_signal[t * in_channels + d] - mean[d])/sqrt(var[d] + eps)) + beta[d];
                 }
             }
         }
         else{
             for(int t = 0; t < in_T ; t++){
                 for(int d = 0 ; d < in_channels ; d++){
-                    output_signal[t * in_channels + d] = gamma[d]*((input_signal[t * in_channels + d] - mean[d])/sqrt(var[d])) + beta[d];
+                    output_signal[t * in_channels + d] = gamma[d]*((input_signal[t * in_channels + d] - mean[d])/sqrt(var[d] + eps)) + beta[d];
                 }
             }
         }
@@ -232,14 +233,14 @@ int BatchNorm1d(float* output_signal, float* input_signal, unsigned in_T, unsign
         if(in_place){
             for(int t = 0; t < in_T ; t++){
                 for(int d = 0 ; d < in_channels ; d++){
-                    input_signal[t * in_channels + d]  = ((input_signal[t * in_channels + d] - mean[d])/sqrt(var[d]));
+                    input_signal[t * in_channels + d]  = ((input_signal[t * in_channels + d] - mean[d])/sqrt(var[d] + eps));
                 }
             }
         }
         else{
             for(int t = 0; t < in_T ; t++){
                 for(int d = 0 ; d < in_channels ; d++){
-                    output_signal[t * in_channels + d] = ((input_signal[t * in_channels + d] - mean[d])/sqrt(var[d]));
+                    output_signal[t * in_channels + d] = ((input_signal[t * in_channels + d] - mean[d])/sqrt(var[d] + eps));
                 }
             }
         }
