a053baad42495c7ce5262833dc62645e3ca89a73
Anirudh0707 <yani.bh@gmail.com>
2020-10-09 17:40:30 +0530

    Documentation of conv source files

--- c_reference/include/conv1d.h
+++ c_reference/include/conv1d.h
@@ -1,42 +1,68 @@
-#include<stdlib.h>
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
 #ifndef __CONVLAYERS_H__
 #define __CONVLAYERS_H__
 
-#define ERR_INTERMIDIATE_NOT_INIT -1
-#define ERR_TEMPW_NOT_INIT -2
-#define ERR_TEMPLRU_NOT_INIT -3
-#define ERR_NORMFEATURES_NOT_INIT -4
-
 /**
  * @brief Model paramters for the 1D Convolution Layer
- * @var       mean         pointer to mean of input vector for normalization, size inputDims
- * @var       stdDev       pointer to standard dev of input for normalization, size inputDims*steps
- * @var       W            pointer to convolutional weights W
- * @var       B            pointer to the bias vector for the convolution
+ * @var   W    pointer to convolutional weights W, size for regular = out_channels*in_channels*kernel_size, size for depth based = out_channels*kernel_size
+ * @var   B    pointer to the bias vector for the convolution, shape = [out_channels]
  */
 typedef struct ConvLayers_Params{
     float* W;
     float* B;
 } ConvLayers_Params;
 
-
+/**
+ * @brief Model definition for the 1D Convolution Layer
+ * @param[out]   output_signal    pointer to the output signal, size = out_T * out_channels
+ * @param[in]    out_T            number of time steps in the output
+ * @param[in]    out_channels     number of output channels for the ouput of the conv layer
+ * @param[in]    input_signal     pointer to the input signal. size = in_T * in_channels
+ * @param[in]    in_T             number of time steps in the input
+ * @param[in]    in_channels      number of input channels
+ * @param[in]    padding          padding applied to the input before the conv is performed. Note: padding is applied to both the start and end
+ * @param[in]    kernel_size      kernel size of the conv filter
+ * @param[in]    params           weights, bias and other essential parameters used to describe the layer
+ * @param[in]    activations      an integer to choose the type of activation function.
+ *                                0: none
+ *                                1: sigmoid
+ *                                2: tanh
+ *                                3: relu
+ */
 int Conv1D(float *output_signal, unsigned out_T, unsigned out_channels, const float *input_signal, 
     unsigned in_T, unsigned in_channels, int padding, unsigned kernel_size, 
     const void* params, int activations);
 
+/**
+ * @brief Model definition for the 1D Depthwise Convolution Layer
+ * @param[out]   output_signal    pointer to the output signal, size = out_T * in_channels
+ * @param[in]    out_T            number of time steps in the output
+ * @param[in]    input_signal     pointer to the input signal. size = in_T * in_channels
+ * @param[in]    in_T             number of time steps in the input
+ * @param[in]    in_channels      number of input channels. The output will have the same number of channels
+ * @param[in]    padding          padding applied to the input before the conv is performed. Note: padding is applied to both the start and end
+ * @param[in]    kernel_size      kernel size of the conv filter
+ * @param[in]    params           weights, bias and other essential parameters used to describe the layer
+ * @param[in]    activations      an integer to choose the type of activation function.
+ *                                0: none
+ *                                1: sigmoid
+ *                                2: tanh
+ *                                3: relu
+ */
 int Conv1D_Depth(float *output_signal, unsigned out_T, const float *input_signal, 
     unsigned in_T, unsigned in_channels, int padding, unsigned kernel_size, 
     const void* params, int activations);
 
-// Low Rank
+
+// Low Rank Convolution
 /**
- * @brief Model paramters for the Low Rank 1D Convolution Layer
- * @var       mean         pointer to mean of input vector for normalization, size inputDims
- * @var       stdDev       pointer to standard dev of input for normalization, size inputDims
- * @var       W1           pointer to first low-rank component of the convolutional weight W
- * @var       W2           pointer to second low-rank component of the convolutional weight W
- * @var       Rank         rank of W matrix
- * @var       B            pointer to the bias vector for the convolution
+ * @brief Model paramters for the 1D Convolution Layer
+ * @var    W1      pointer to the 1st low-rank component of the weights, size = out_channels * rank
+ * @var    W2      pointer to the 2nd low-rank component of the weights, size for regular = rank * in_channels * kernel_size, size for depthwise = rank * kernel_size
+ * @var    B       pointer to the bias vector for the convolution, shape = [out_channels]
+ * @var    rank    rank of the weight tensor. A low rank decomposition typically used to reduce computation and storage
  */
 typedef struct ConvLayers_LR_Params{
     float* W1;
@@ -45,18 +71,82 @@ typedef struct ConvLayers_LR_Params{
     unsigned rank;
 } ConvLayers_LR_Params;
 
+/**
+ * @brief Model definition for the 1D Low Rank Convolution Layer
+ * @brief Identical to the non-low-rank form. One modification is the mulitplication of the weights handeled witin the layer
+ * @param[out]   output_signal    pointer to the output signal, size = out_T * out_channels
+ * @param[in]    out_T            number of time steps in the output
+ * @param[in]    out_channels     number of output channels for the ouput of the conv layer
+ * @param[in]    input_signal     pointer to the input signal. size = in_T * in_channels
+ * @param[in]    in_T             number of time steps in the input
+ * @param[in]    in_channels      number of input channels
+ * @param[in]    padding          padding applied to the input before the conv is performed. Note: padding is applied to both the start and end
+ * @param[in]    kernel_size      kernel size of the conv filter
+ * @param[in]    params           weights, bias and other essential parameters used to describe the layer
+ * @param[in]    activations      an integer to choose the type of activation function.
+ *                                0: none
+ *                                1: sigmoid
+ *                                2: tanh
+ *                                3: relu
+ */
 int Conv1D_LR(float *output_signal, unsigned out_T, unsigned out_channels, const float *input_signal, 
     unsigned in_T, unsigned in_channels, int padding, unsigned kernel_size, 
     const void* params, int activations);
 
+/**
+ * @brief Model definition for the 1D Depthwise Convolution Layer
+ * @brief Identical to the non-low-rank form. One modification is the mulitplication of the weights handeled witin the layer
+ * @param[out]   output_signal    pointer to the output signal, size = out_T * in_channels
+ * @param[in]    out_T            number of time steps in the output
+ * @param[in]    input_signal     pointer to the input signal. size = in_T * in_channels
+ * @param[in]    in_T             number of time steps in the input
+ * @param[in]    in_channels      number of input channels. The output will have the same number of channels
+ * @param[in]    padding          padding applied to the input before the conv is performed. Note: padding is applied to both the start and end
+ * @param[in]    kernel_size      kernel size of the conv filter
+ * @param[in]    params           weights, bias and other essential parameters used to describe the layer
+ * @param[in]    activations      an integer to choose the type of activation function.
+ *                                0: none
+ *                                1: sigmoid
+ *                                2: tanh
+ *                                3: relu
+ */
 int Conv1D_Depth_LR(float *output_signal, unsigned out_T, const float *input_signal, 
     unsigned in_T, unsigned in_channels, int padding, unsigned kernel_size, 
     const void* params, int activations);
 
 // Auxillary Layers
+/**
+ * @brief Model definition for the 1D Average Pooling Layer
+ * @param[out]   output_signal    pointer to the output signal, size = out_T * in_channels. Provide Null/0 incase of inplace computation
+ * @param[in]    out_T            number of time steps in the output
+ * @param[in]    input_signal     pointer to the input signal. size = in_T * in_channels
+ * @param[in]    in_T             number of time steps in the input
+ * @param[in]    in_channels      number of input channels. The output will have the same number of channels
+ * @param[in]    padding          padding applied to the input before the pool is performed. Note: padding is applied to both the start and end
+ * @param[in]    kernel_size      kernel size of the pool filter
+ * @param[in]    activations      an integer to choose the type of activation function.
+ *                                0: none
+ *                                1: sigmoid
+ *                                2: tanh
+ *                                3: relu
+ */
 int AvgPool1D(float *output_signal, unsigned out_T, const float *input_signal, unsigned in_T, unsigned in_channels, 
     int padding, unsigned kernel_size, int activations);
 
+/**
+ * @brief Model definition for the 1D batch Normalization Layer
+ * @param[out]   output_signal    pointer to the output signal, size = out_T * in_channels. Provide Null/0 incase of inplace computation
+ * @param[in]    input_signal     pointer to the input signal. size = in_T * in_channels
+ * @param[in]    in_T             number of time steps in the input
+ * @param[in]    in_channels      number of input channels. The output will have the same number of channels
+ * @param[in]    mean             pointer to the mean for the batch normalization, size = in_channels
+ * @param[in]    var              pointer to the variance for the batch normalization, size = in_channels
+ * @param[in]    affine           whether the affine operations are applied
+ * @param[in]    gamma            pointer to the scaling factors for the post-norm affine operation, size = in_channels
+ * @param[in]    beta             pointer to the scalar offsets for the post-norm affine operation, size = in_channels
+ * @param[in]    in_place         in place computation of the batchnorm i.e. the output is stored in place of the input signal. Storage efficient
+ * @param[in]    eps              a very small +ve value to avoid division by 0. For the default value, assign = 0.00001
+ */
 int BatchNorm1d(float* output_signal, float* input_signal, unsigned in_T, unsigned in_channels, 
-    float* mean, float* var, unsigned affine, float* gamma , float * beta, unsigned in_place);
+    float* mean, float* var, unsigned affine, float* gamma , float * beta, unsigned in_place, float eps);
 #endif
--- c_reference/include/dscnn.h
+++ c_reference/include/dscnn.h
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
 #ifndef __DSCNN__
 #define __DSCNN__
 
@@ -6,10 +9,80 @@
 #include<stdlib.h>
 #include<math.h>
 
+/**
+ * @brief Model definition for the 1D Convolution sub-block applied before the RNN
+ * @brief sub-layers : BatchNorm1d -> Conv1D_LR
+ * 
+ * @param[out]   output_signal       pointer to the final output signal, minimum size = out_T * in_channels. out_T has to be calculated based on the reduction from all the conv and pool layers
+ * @param[in]    input_signal        pointer to the input signal. size = in_T * in_channels
+ * @param[in]    in_T                number of time steps in the input
+ * @param[in]    in_channels         number of input channels. The output will have the same number of channels
+ 
+ * @param[in]    mean                pointer to the mean for the batch normalization, size = in_channels
+ * @param[in]    var                 pointer to the variance for the batch normalization, size = in_channels
+ * @param[in]    affine              whether the affine operations are applied
+ * @param[in]    gamma               pointer to the scaling factors for the post-norm affine operation, size = in_channels
+ * @param[in]    beta                pointer to the scalar offsets for the post-norm affine operation, size = in_channels
+ * @param[in]    in_place            in place computation of the batchnorm. Storage efficient
+ * 
+ * @param[in]    cnn_hidden          hidden state/out_channels dimensions for the CNN
+ * @param[in]    cnn_padding         padding for the CNN layer. Note: applied to both sides of the input 
+ * @param[in]    cnn_kernel_size     kernel size of the CNN
+ * @param[in]    cnn_params          weights, bias and other essential parameters used to describe the CNN
+ * @param[in]    cnn_activations     an integer to choose the type of activation function.
+ *                                   0: none
+ *                                   1: sigmoid
+ *                                   2: tanh
+ *                                   3: relu
+ */
 int DSCNN_LR(float* output_signal, float* input_signal, unsigned in_T, unsigned in_channels, float* mean, float* var,
     unsigned affine, float* gamma, float* beta, unsigned in_place, unsigned cnn_hidden, int cnn_padding, unsigned cnn_kernel_size,
     const void* cnn_params, int cnn_activations);
 
+/**
+ * @brief Model definition for the 1D Convolution sub-block applied after the RNN
+ * @brief sub-layers : TanhGate(custom) nonlinearity -> BatchNorm1d -> Conv1D_Depth -> Conv1d_LR -> AvgPool
+ * 
+ * @param[out]   output_signal          pointer to the final output signal, minimum size = out_T * in_channels. out_T has to be calculated based on the reduction from all the conv and pool layers
+ * @param[in]    input_signal           pointer to the input signal. size = in_T * in_channels
+ * @param[in]    in_T                   number of time steps in the input
+ * @param[in]    in_channels            number of input channels. The output will have the same number of channels
+ 
+ * @param[in]    mean                   pointer to the mean for the batch normalization, size = in_channels
+ * @param[in]    var                    pointer to the variance for the batch normalization, size = in_channels
+ * @param[in]    affine                 whether the affine operations are applied
+ * @param[in]    gamma                  pointer to the scaling factors for the post-norm affine operation, size = in_channels
+ * @param[in]    beta                   pointer to the scalar offsets for the post-norm affine operation, size = in_channels
+ * @param[in]    in_place               in place computation of the batchnorm. Storage efficient
+ * 
+ * @param[in]    depth_cnn_hidden       hidden state/out_channels dimensions for the depth CNN
+ * @param[in]    depth_cnn_padding      padding for the depth CNN layer. Note: applied to both sides of the input 
+ * @param[in]    depth_cnn_kernel_size  kernel size of the depth CNN
+ * @param[in]    depth_cnn_params       weights, bias and other essential parameters used to describe the depth CNN
+ * @param[in]    depth_cnn_activations  an integer to choose the type of activation function.
+ *                                      0: none
+ *                                      1: sigmoid
+ *                                      2: tanh
+ *                                      3: relu
+ * 
+ * @param[in]    point_cnn_hidden       hidden state/out_channels dimensions for the point CNN
+ * @param[in]    point_cnn_padding      padding for the point CNN layer. Note: applied to both sides of the input 
+ * @param[in]    point_cnn_kernel_size  kernel size of the point CNN
+ * @param[in]    point_cnn_params       weights, bias and other essential parameters used to describe the point CNN
+ * @param[in]    point_cnn_activations  an integer to choose the type of activation function.
+ *                                      0: none
+ *                                      1: sigmoid
+ *                                      2: tanh
+ *                                      3: relu
+ * 
+ * @param[in]    pool_padding           padding for the pool layer. Note: applied to both sides of the input 
+ * @param[in]    pool_kernel_size       kernel size of the pool
+ * @param[in]    pool_activations       an integer to choose the type of activation function.
+ *                                      0: none
+ *                                      1: sigmoid
+ *                                      2: tanh
+ *                                      3: relu
+ */
 int DSCNN_LR_Point_Depth(float* output_signal, float* input_signal, unsigned in_T, unsigned in_channels, float* mean, float* var,
     unsigned affine, float* gamma, float* beta, unsigned in_place, unsigned depth_cnn_hidden, int depth_cnn_padding, 
     unsigned depth_cnn_kernel_size, const void* depth_cnn_params, int depth_cnn_activations, unsigned point_cnn_hidden, 
