1456955be19b7b3f2eaabbabeb02d322e680e812
Anirudh0707 <yani.bh@gmail.com>
2020-10-09 17:56:56 +0530

    minor modifications to conv test files

--- c_reference/tests/conv1d/conv1d_regular/test_conv1d.c
+++ c_reference/tests/conv1d/conv1d_regular/test_conv1d.c
@@ -15,13 +15,16 @@ int main(){
     float pred[O_T * O_F] = {};
     Conv1D(pred, O_T, O_F, INPUT, I_T, I_F, PAD, FILT, &conv_params, ACT);
     float error = 0;
+    float denom = 0;
     for(int t = 0 ; t < O_T ; t++){
         for(int d = 0 ; d < O_F ; d++){
             error += ((pred[t * O_F + d] - OUTPUT[t * O_F + d]) * (pred[t * O_F + d] - OUTPUT[t * O_F + d]));
+            denom += OUTPUT[t * O_F + d] * OUTPUT[t * O_F + d] ;
         }
     }
     float avg_error = error/(O_T*O_F);
+    printf("Testing Regular Convolution\n");
     printf("Squared Error : %f \t ; MSE : %f  \n", error, avg_error);
-
+    printf("Relative Squared Error : %f", error/denom)
     return 0 ;
 }
