07b6d310f2be0fb7254eba8652d2c1fed11ba44a
Anirudh0707 <yani.bh@gmail.com>
2020-10-19 14:55:03 +0530

    Reduce conv1d test-bench size

--- c_reference/tests/conv1d/conv1d_lr_depthwise/conv_param_lr_depth.h
+++ c_reference/tests/conv1d/conv1d_lr_depthwise/conv_param_lr_depth.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1e44aba5fb5b03b601060e6ebacfe9047132825f711431bcaf3279a582425151
-size 1948109
+oid sha256:ec7371939356f289e0f55fcbd6a6dfa21d3c25ca4f4a8ae624122ee0f5334a44
+size 55497
